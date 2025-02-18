3144c5939e32a292ae28456d57212ce9cf88d0ec
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-02-18 15:16:47 +0100

    add function for btn when clicked

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-const lampOff = document.querySelector('.lampOff')
-const lampOn = document.querySelector('.lampOn')
+const lampOff = document.querySelector('.lamp_off')
+const lampOn = document.querySelector('.lamp_on')
+const btn = document.querySelector('button')
+
+lampOn.classList.add('inactive');
+lampOff.classList.add('active')
+
+btn.addEventListener('click', function (){
+    lampOn.classList.remove('inactive')
+    lampOn.classList.add('active')
+    lampOff.classList.remove('active')
+    lampOff.classList.add('inactive')
+})
