bde9632fa3bbe299667c8821900d938da634dbae
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-02-18 16:11:11 +0100

    fixed function for btn

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
 const lampOff = document.querySelector('.lamp_off')
 const lampOn = document.querySelector('.lamp_on')
 const btn = document.querySelector('button')
+const arrButton = [btn.textContent]
 
 lampOn.classList.add('inactive');
-lampOff.classList.add('active')
+lampOff.classList.add('active');
 
 btn.addEventListener('click', function (){
-    lampOn.classList.remove('inactive')
-    lampOn.classList.add('active')
-    lampOff.classList.remove('active')
-    lampOff.classList.add('inactive')
+    if (arrButton.includes('Accendi')){
+        lampOn.classList.remove('inactive')
+        lampOn.classList.add('active')
+        lampOff.classList.remove('active')
+        lampOff.classList.add('inactive')
+        console.log(lampOff.classList);
+        console.log(lampOn.classList);
+        
+        btn.innerText = 'Spegni'
+    }
+    else if (arrButton.includes('Spegni')){
+        lampOn.classList.remove('active')
+        lampOn.classList.add('inactive')
+        lampOff.classList.remove('inactive')
+        lampOff.classList.add('active')
+        console.log(lampOff.classList);
+        console.log(lampOn.classList);
+
+        btn.innerText = 'Accendi'
+    }
 })
+
